4115631d6a01e93b555530655c199677d639aba9
girl-codes <168541859+girl-codes@users.noreply.github.com>
2024-05-01 18:12:37 +0100

    Integrated the Weather API into JS
    - Integrated the SheCodes Weather API into JS so that the correct temperature is shown for the city that is searched
    - Removed the original city and temperature so that when the page initially loads - the correct temperature is shown for the city of  'London'

--- src/interaction.js
+++ src/interaction.js
@@ -1,8 +1,26 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature) + "°C";
+}
+
+function searchCity(city) {
+  let apiKey = "f12c79ob5a3b7240208d7843e2380dt7";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiURL).then(updateWeather);
+}
+
 function actionSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 let searchformElement = document.querySelector("#search-form");
 searchformElement.addEventListener("submit", actionSearchSubmit);
+
+searchCity("London");
