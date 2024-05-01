86f4fb604f3be73936e8ba1dfa4d0939ce09f9f8
girl-codes <168541859+girl-codes@users.noreply.github.com>
2024-05-01 17:25:19 +0100

    Added basic search engine JS
    - Added JS so that the h1 changes to whichever city is searched in the box
    - Added autocomplete (off) so that previous suggestions don't come up when someone searches a city - make it look cleaner

---
+++ src/interaction.js
@@ -0,0 +1,8 @@
+function actionSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+let searchformElement = document.querySelector("#search-form");
+searchformElement.addEventListener("submit", actionSearchSubmit);
